c976a4d917b34d10b1ad062849e2bc46fbf3c5e4
SeptiRizki <75733090+SeptiRizki@users.noreply.github.com>
2024-01-10 02:02:08 +0700

    Update footer.blade.php

--- resources/views/admin-lte/footer.blade.php
+++ resources/views/admin-lte/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
     <div class="row justify-content-center">
-      <strong>Copyright &copy; Musyahya 2021</strong>
+      <strong>Copyright &copy; Perpustakaan 2024</strong>
     </div>
 </footer>
